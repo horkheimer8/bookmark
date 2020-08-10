f70fc061d341e43a00bafec4ff7318d88a9ba4d9
egoing <horkheimer8@gmail.com>
2020-08-10 12:39:59 +0900

    dsf

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 
-def main():
+if __name__ == "__main__":
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
@@ -14,9 +14,6 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        )from exc
+        )
     execute_from_command_line(sys.argv)
 
-
-if __name__ == '__main__':
-    main()
